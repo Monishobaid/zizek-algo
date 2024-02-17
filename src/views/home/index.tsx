b2297f6b05958ea2c0e007ef5f04f263dc3b8588
Monishobaid <monishobaid@gmail.com>
2024-02-17 21:17:46 +0530

    added zizek images and working on horizontal scroll

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,25 @@
 import AnimatedTextCharacter from "../../components/Animation/textanimated";
+import ZizekImage from "../../assets/images/zizekdoodle.jpeg";
+// import HorizontalScroll  from "../../components/Animation/scrollHorizontal"; // Importing the custom hook
 
 export default function About() {
+  // const horizontalScrollRef = useHorizontalScroll(); // Using the custom hook
+
   return (
-    <div>
-      <div className="font-zeyada">
-        <AnimatedTextCharacter text="Welcome to Zizek Algo" />
+    <main>
+      <div className="flex flex-col h-full overflow-x-auto">
+        <div className="flex justify-center items-center font-shadows">
+          <AnimatedTextCharacter text="Welcome to Zizek Algo" />
+        </div>
+        <div className="flex justify-center items-center font-zeyada">
+          <img src={ZizekImage} alt="Zizek Doodle" />
+        </div>
       </div>
-    </div>
+      <div className="flex flex-col h-full">
+        <div>
+          <p>Hello world</p>
+        </div>
+      </div>
+    </main>
   );
 }
