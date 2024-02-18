f611c6d8f714d93fa83a0e5e8e723aa6a95dc1ee
Monishobaid <monishobaid@gmail.com>
2024-02-19 04:23:22 +0530

    added scroll animation

---
+++ src/components/Animation/SmoothScroll.tsx
@@ -0,0 +1,90 @@
+import React, {
+    useRef,
+    useState,
+    useLayoutEffect,
+    useCallback,
+    useEffect
+  } from "react";
+  import ResizeObserver from "resize-observer-polyfill";
+  import {
+    motion,
+    useTransform,
+    useSpring,
+    useMotionValue
+  } from "framer-motion";
+  
+  import { useScrollPercentage } from "react-scroll-percentage";
+  
+  const SmoothScroll = () => {
+    const scrollRef = useRef(null);
+    const ghostRef = useRef(null);
+    const [scrollRange, setScrollRange] = useState(0);
+    const [viewportW, setViewportW] = useState(0);
+  
+    const scrollPerc = useMotionValue(0);
+  
+    useLayoutEffect(() => {
+      const element = scrollRef.current as unknown as HTMLElement;
+      if (element) {
+        setScrollRange(element.scrollWidth);
+      }
+    }, [scrollRef]);
+    
+  
+    const onResize = useCallback((entries: ResizeObserverEntry[]) => {
+      for (let entry of entries) {
+        setViewportW(entry.contentRect.width);
+      }
+    }, []);
+  
+    useLayoutEffect(() => {
+      const resizeObserver = new ResizeObserver((entries) => onResize(entries));
+      if (ghostRef.current) {
+        resizeObserver.observe(ghostRef.current);
+        return () => resizeObserver.disconnect();
+      }
+    }, [onResize]);
+    
+  
+  
+    const [containerRef, percentage] = useScrollPercentage({
+      /* Optional options */
+      threshold: 0.1
+    });
+  
+    useEffect(() => {
+      scrollPerc.set(percentage);
+    }, [percentage]);
+  
+    const transform = useTransform(
+      scrollPerc,
+      [0.1, 0.8],
+      [0, -scrollRange + viewportW]
+    );
+    const physics = { damping: 1, mass: 0.20, stiffness: 2 };
+    const spring = useSpring(transform, physics);
+  
+    return (
+      <div ref={containerRef}>
+        <div className="sticky top-0 left-0 right-0 will-change-transform">
+          {percentage}
+  
+          <motion.section
+            ref={scrollRef}
+            style={{ x: spring }}
+            className="relative h-screen w-screen flex items-center px-40 bg-yellow-500 border-40 border-yellowGreen"
+          >
+            <div className="relative flex border-20 bg-pink-500">
+              <div className="h-screen w-screen border-15 bg-red-500">hello world 2</div>
+              <div className="h-screen w-screen border-15 bg-green-500">Hello world 3</div>
+            </div>
+          </motion.section>
+        </div>
+        <div ref={ghostRef} style={{ height: scrollRange }} className="ghost" />
+      </div>
+    );
+  };
+  
+  export default SmoothScroll;
+
+  
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,13 +1,29 @@
 import AnimatedTextCharacter from "../../components/Animation/textanimated";
 import ZizekImage from "../../assets/images/zizekdoodle.jpeg";
 // import HorizontalScroll  from "../../components/Animation/scrollHorizontal"; // Importing the custom hook
+import SmoothScroll from "../../components/Animation/SmoothScroll";
 
 export default function About() {
   // const horizontalScrollRef = useHorizontalScroll(); // Using the custom hook
 
   return (
-    <main>
-      <div className="flex flex-col h-full overflow-x-auto">
+    // <main>
+    //   <div className="flex flex-col h-full overflow-x-auto">
+    //     <div className="flex justify-center items-center font-shadows">
+    //       <AnimatedTextCharacter text="Welcome to Zizek Algo" />
+    //     </div>
+    //     <div className="flex justify-center items-center font-zeyada">
+    //       <img src={ZizekImage} alt="Zizek Doodle" />
+    //     </div>
+    //   </div>
+    //   <div className="flex flex-col h-full">
+    //     <div>
+    //       <p>Hello world</p>
+    //     </div>
+    //   </div>
+    // </main>
+    <>
+      <div className="flex flex-col h-screen">
         <div className="flex justify-center items-center font-shadows">
           <AnimatedTextCharacter text="Welcome to Zizek Algo" />
         </div>
@@ -15,11 +31,8 @@ export default function About() {
           <img src={ZizekImage} alt="Zizek Doodle" />
         </div>
       </div>
-      <div className="flex flex-col h-full">
-        <div>
-          <p>Hello world</p>
-        </div>
-      </div>
-    </main>
+      <SmoothScroll />
+      <div className="h-screen"></div>
+    </>
   );
 }
